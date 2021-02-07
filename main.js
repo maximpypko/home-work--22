e0acd3eae02c3ca7524804a07dc56660b4aea258
Maxim Pypko <59706496+maximpypko@users.noreply.github.com>
2021-02-07 17:32:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function reverseString(string, result = '') {
 reverseString('Надо сделать домашку');
 
 ///////////////////////////////////////////////////
-/////////////////////0   1   2   3   4   5   6   7   8   9   10  11 12  13  14  15  16
+
 const arrNumbers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'];
 
 function getItem(arr, letter, start, end) {
